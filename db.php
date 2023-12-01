689e5e58a0aa2fcd5abcbdc3be173c3a89ec1e14
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-12-01 13:57:10 +0000

    Initial commit

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "data_encryption";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to the database successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
